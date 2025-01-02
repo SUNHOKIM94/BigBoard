637dee074da80bc803091cc9d17ab8e4070fc8bc
kimsunho940904 <hkkbc123@gmail.com>
2025-01-02 14:42:40 +0900

    1. 프로젝트 초기 구성

---
+++ service/view/src/main/java/kuke/board/view/VIewApplication.java
@@ -0,0 +1,14 @@
+package kuke.board.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VIewApplication {
+
+	public static void main( String[] args ) {
+
+		SpringApplication.run( VIewApplication.class, args );
+	}
+
+}
